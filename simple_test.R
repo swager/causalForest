7c31a803a7d03065c29079467b020aa1c6057c22
Stefan Wager <swager@stanford.edu>
2015-10-31 16:49:00 -0700

    Delete some unsupported functions.

--- simple_test.R
+++ simple_test.R
@@ -1,3 +1,4 @@
+library(causalForest)
 n = 500
 p = 10
 X = matrix(rnorm(n * p), n, p)
@@ -5,4 +6,6 @@ Y = rnorm(n)
 W = rbinom(n, 1, 0.5)
 
 ct = causalTree(Y ~ X, treatment=W, split.option="CT", cv.option="TOT")
-hist(predict(ct))
+summary(ct)
+min(predict(ct))
+max(predict(ct))
